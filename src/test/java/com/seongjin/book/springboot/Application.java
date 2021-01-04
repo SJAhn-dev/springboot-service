ae5b5ab823ccabacee3bbcd97f4c3d080381cc18
SeongJin.dev <seongjin.dev@gmail.com>
2021-01-04 22:08:43 +0900

    Add JUnit4 TestCode

---
+++ src/test/java/com/seongjin/book/springboot/Application.java
@@ -0,0 +1,16 @@
+package com.seongjin.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
